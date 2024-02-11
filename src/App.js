2f77e7de7194c63933b6536a2040b9302955edd2
ashenshandeepoffl <ashenshandeepoffl@gmail.com>
2024-02-11 23:44:03 +0530

    React Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Ashen";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User name = "Ashen" age = {22} email = "ashen@gmail.com"/>
+      <User name = "Zaynab" age = {22} email = "zaynab@gmail.com"/>
     </div>
   );
+}
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
+    </div>
+  )
 }
 
 export default App;
